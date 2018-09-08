ba2924c49507b8f0acc7e2a2ca1c9d9dc08dee6d
flische <flische@gmail.com>
2018-09-07 18:00:00 -0700

    completed send messages functionality

--- src/components/chat/chat.js
+++ src/components/chat/chat.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { db } from '../../firebase';
 import { connect } from 'react-redux';
 import { updateChat } from '../../actions';
+import { reduxForm, Field } from 'redux-form';
+import Input from '../input';
 import './chat.css';
 
 class Chat extends Component {
@@ -15,9 +17,19 @@ class Chat extends Component {
     componentWillUnmount(){
         this.dbRef.off();
     }
+    sendMessage = async ({ message }) => {
+        const newMessage = {
+            name: localStorage.getItem('name'),
+            message
+        };
+
+        await this.dbRef.push(newMessage);
+
+        this.props.reset();
+    }
 
     render(){
-        const { log } = this.props;
+        const { log, handleSubmit } = this.props;
 
         const ChatElements = Object.keys(log).map(k => {
             const { name, message } = log[k];
@@ -32,8 +44,14 @@ class Chat extends Component {
         return (
             <div>
                 <h1 className="center">Chat Room</h1>
-                <div className="row right-align">
+                <div className="row center">
                     <Link to="/" className="btn orange darken-4">Home</Link>
+                </div>
+                <p>Hello, {localStorage.getItem('name')}</p>
+                <div className="row">
+                    <form onSubmit={handleSubmit(this.sendMessage)} className="col s8 offset-s2">
+                        <Field name="message" label="Message" component={Input}/>
+                    </form>
                 </div>
                 <ul className="collection">
                     {ChatElements}
@@ -43,11 +61,16 @@ class Chat extends Component {
     }
 }
 
+
+Chat = reduxForm({
+    form: 'text-message',
+    validate: ({message}) => message ? {} : {message: 'No empty messages!'}
+})(Chat);
+
 function mapStateToProps(state){
     return {
         log: state.chat.log
     }
 }
-
 
 export default connect(mapStateToProps, { updateChat })(Chat);
